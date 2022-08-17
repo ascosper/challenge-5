81e2872173c298a90c0773bebc8dbe8e4583bc87
ascosper@hotmail.com <Amaya@Normas-MacBook-Air.local>
2022-08-17 18:06:51 -0500

    time Slots

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,28 @@
 //connect time api
 var todayDate = moment().format('LLLL'); 
 $("#currentDay").html(todayDate)
-function getData() {
-    const response =  fetch('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js')
-    const data = response.json()
-  }
-//connect for past prest future
-function getData() {
-    const response =  fetch('https://code.jquery.com/jquery-3.4.1.min.js')
-    const data = response.json()
-  }
-  var past = document.getElementsByClassName ('past');
-  var present = document.getElementsByClassName ('present');
-  var future = document.getElementsByClassName ('future');
 
+//connect for past prest future
+
+
+const rows = document.getElementsByClassName("row");
+let currentHour = parseInt(moment().format('H'));
+
+Array.from(rows).forEach(row => {
+  let
+    rowIdString = row.id,
+    rowHour;
+  if (rowIdString) {
+    rowHour = parseInt(rowIdString);
+  }
+  if (rowHour) {
+    if (currentHour > rowHour) {
+      addClass(past);
+    } else if (currentHour < rowHour) {
+        removeClass(past)
+        addClass(future)
+  }}
+});
 
 //save button after reload
 
