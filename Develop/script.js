3d822b7d9a55878542bba1c16c355419716c28f6
ascosper@hotmail.com <Amaya@Normas-Air.attlocal.net>
2022-08-17 14:10:26 -0500

    javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,16 @@
 //connect time api
+function getData() {
+    const response =  fetch('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js')
+    const data = response.json()
+  }
 //connect for past prest future
+function getData() {
+    const response =  fetch('https://code.jquery.com/jquery-3.4.1.min.js')
+    const data = response.json()
+  }
+  var past = document.getElementsByClassName ('past');
+  var present = document.getElementsByClassName ('present');
+  var future = document.getElementsByClassName ('future');
+  
 
 //save button after reload
-  
